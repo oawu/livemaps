b58064c186b0c37c9f979c964d835bd8ddf69e06
OA Wu <comdan66@gmail.com>
2016-10-13 16:51:31 +0800

    Copy ignore files.

---
+++ ignore_files/config.js
@@ -0,0 +1,13 @@
+/**
+ * @author      OA Wu <comdan66@gmail.com>
+ * @copyright   Copyright (c) 2016 OA Wu Design
+ * @website     http://www.ioa.tw/
+ */
+
+window.firebaseConfig = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: '',
+  storageBucket: '',
+  messagingSenderId: ''
+};
