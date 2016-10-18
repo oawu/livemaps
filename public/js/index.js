ae54ae81199fd8a078b65101af8c38396fd69d33
OA Wu <comdan66@gmail.com>
2016-10-18 08:56:45 +0800

    Tmp.

--- public/js/index.js
+++ public/js/index.js
@@ -197,7 +197,7 @@ $(function () {
   window.vars.audio = { pop: new Audio('pop.mp3'), chat: new Audio('chat.mp3')};
 
 
-  window.funcs.initFirebase (window.storages.version.get (15));
+  window.funcs.initFirebase (window.storages.version.get (14));
   window.vars.$.popbox.find ('.cover, .cancel').click (function () { window.vars.$.popbox.removeClass ('show'); });
 
   google.maps.event.addDomListener (window, 'load', function () {
